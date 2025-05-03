c177839669093628b305115aec44fc831afc163e
romandom <dominik.roman@bluelemons.sk>
2025-05-03 18:05:55 +0200

    port

--- src/main/java/cz/diplomka/pivovar/arduino/ArduinoConfig.java
+++ src/main/java/cz/diplomka/pivovar/arduino/ArduinoConfig.java
@@ -16,7 +16,7 @@ public class ArduinoConfig {
     public SerialPort serialPort() {
         //serialPort = SerialPort.getCommPort("COM4");
         serialPort = SerialPort.getCommPort("/dev/ttyACM0");
-        serialPort.setComPortParameters(250000, 8, 1, 0);
+        serialPort.setComPortParameters(9600, 8, 1, 0);
         serialPort.setComPortTimeouts(SerialPort.TIMEOUT_WRITE_BLOCKING, 0, 0);
 
         if (serialPort.openPort()) {
